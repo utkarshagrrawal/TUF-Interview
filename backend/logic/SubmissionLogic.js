a4f4a86522053d613a65d607ea2ace026a2c33f5
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-03-20 10:31:17 +0530

    Added error handling in backend

--- backend/logic/SubmissionLogic.js
+++ backend/logic/SubmissionLogic.js
@@ -4,28 +4,40 @@ const { dbConnection } = require("../utility/DatabaseUtility");
 const submitCodeLogic = async (body) => {
     const { username, language, stdin, code } = body;
 
-    const result = await new Promise((resolve, reject) => {
-        dbConnection.query('INSERT INTO Submissions (username, language, stdin, code) VALUES (?, ?, ?, ?)', [username, language, stdin, code], (err, result) => {
-            if (err) {
-                reject({ error: err });
-            }
-            resolve({ success: true });
+    var result;
+
+    try {
+        result = await new Promise((resolve, reject) => {
+            dbConnection.query('INSERT INTO Submissions (username, language, stdin, code) VALUES (?, ?, ?, ?)', [username, language, stdin, code], (err, result) => {
+                if (err) {
+                    reject({ error: err });
+                }
+                resolve({ success: true });
+            })
         })
-    })
+    } catch (err) {
+        result = { error: err }
+    }
 
     return result;
 }
 
 
 const fetchSubmissionsLogic = async () => {
-    const result = await new Promise((resolve, reject) => {
-        dbConnection.query('SELECT * FROM Submissions', (err, rows) => {
-            if (err) {
-                reject({ error: err });
-            }
-            resolve({ submissions: rows });
+    var result;
+
+    try {
+        result = await new Promise((resolve, reject) => {
+            dbConnection.query('SELECT * FROM Submissions', (err, rows) => {
+                if (err) {
+                    reject({ error: err });
+                }
+                resolve({ submissions: rows });
+            })
         })
-    })
+    } catch (err) {
+        result = { error: err }
+    }
 
     return result;
 }
